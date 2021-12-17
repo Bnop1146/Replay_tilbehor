44ae6adc6e173305abb6f0ed245a08d74d7eb797
Joachim <joachim.berner@hotmail.com>
2021-12-17 09:04:15 +0100

    Gået Live

--- settings/init.php
+++ settings/init.php
@@ -3,11 +3,11 @@ require "classes/classDB.php";
 
 const CONFIG_LIVE = "0"; // 0: Test enviroment || 1: Live enviroment
 
-if(CONFIG_LIVE == 0){
-    $DB_SERVER = "localhost";
-    $DB_NAME = "tilbehor";
-    $DB_USER = "root";
-    $DB_PASS = "";
+if(CONFIG_LIVE == 1){
+    $DB_SERVER = "mysql109.unoeuro.com";
+    $DB_NAME = " bnopone_dk_db_tilbehor";
+    $DB_USER = "bnopone_dk";
+    $DB_PASS = "k26RgmedhF4H";
 }else{
     $DB_SERVER = "";
     $DB_NAME = "";
